7d05317d508b7979bd959ebaff7f487a9de3fa73
ronaldyonggi <ronaldyonggi@gmail.com>
2025-03-14 22:04:48 +0800

    initial app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Task } from './task/task.entity';
+import { TaskModule } from './task/task.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT ?? '5432', 10),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      entities: [Task],
+      synchronize: true,
+      autoLoadEntities: true
+    }),
+    TaskModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
