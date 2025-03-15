e7f219b1035372092e0f3f30a651f5fd3fbfdcc0
ronaldyonggi <ronaldyonggi@gmail.com>
2025-03-15 13:12:40 +0800

    service now uses dto

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,7 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Task } from './task.entity';
 import { Repository } from 'typeorm';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
 
 @Injectable()
 export class TaskService {
@@ -14,23 +16,46 @@ export class TaskService {
     return this.taskRepository.find();
   }
 
-  async create(task: Partial<Task>): Promise<Task> {
-    const newTask = this.taskRepository.create(task);
-    return this.taskRepository.save(newTask);
-  }
-
-  async update(id: number, task: Partial<Task>): Promise<Task> {
-    await this.taskRepository.update(id, task);
-    const updatedTask = await this.taskRepository.findOne({
+  async findOne(id: number): Promise<Task> {
+    const task = await this.taskRepository.findOne({
       where: { id },
     });
-    if (!updatedTask) {
-      throw new Error(`Task not found!`);
+    if (!task) {
+      throw new NotFoundException(`Task with id ${id} not found`);
     }
-    return updatedTask;
+
+    return task;
+  }
+
+  /**
+   *  Create a new task, takes in a createTaskDto
+   */
+  async create(createTaskDto: CreateTaskDto): Promise<Task> {
+    const newTask = this.taskRepository.create(createTaskDto); // Create first
+    return this.taskRepository.save(newTask); // Then save to DB
+  }
+
+  /**
+   * Update an existing task, takes in an updateTaskDto
+   */
+  async update(id: number, updateTaskDto: UpdateTaskDto): Promise<Task> {
+    const task = await this.taskRepository.findOne({
+      where: { id },
+    });
+
+    if (!task) {
+      throw new NotFoundException(`Task with id ${id} not found`);
+    }
+
+    // Efficiently merge the changes from updateTaskDto into the existing task entity
+    Object.assign(task, updateTaskDto); 
+    return this.taskRepository.save(task); // Then save to DB
   }
 
   async remove(id: number): Promise<void> {
-    await this.taskRepository.delete(id);
+    const result = await this.taskRepository.delete(id);
+    if (result.affected === 0) {
+      throw new NotFoundException(`Task with id ${id} not found`);
+    }
   }
 }
